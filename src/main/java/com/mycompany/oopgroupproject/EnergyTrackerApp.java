17b843638e48197416ad0ccba1db1056009c77c4
Ying Hui <your-email@example.com>
2026-06-30 17:17:54 +0800

    main menu edit

--- src/main/java/com/mycompany/oopgroupproject/EnergyTrackerApp.java
+++ src/main/java/com/mycompany/oopgroupproject/EnergyTrackerApp.java
@@ -37,47 +37,11 @@ public class EnergyTrackerApp {
                 switch (choice) {
 
                     case 1:
-                        System.out.println("Select Appliance Category to Instantiate: ");
-                        System.out.println("1. Light Appliance");
-                        System.out.println("1. Cooling Appliance");
-                        System.out.println("Enter Your Selection(1-2): ");
-                        int typeChoice = input.nextInt();
-                         
-                        System.out.println("Enter appliance name: ");
-                        String name = input.nextLine();
-                        
-                        System.out.println("Enter Power Rating (Watts):  ");
-                        double watts = Double.parseDouble(input.nextLine());
-                        
-                        System.out.println("Enter Estimated Daily Usage (Hours):  ");
-                        double hours = Double.parseDouble(input.nextLine());
-                        
-                        if (typeChoice == 1){
-                            //light
-                            System.out.println("Enter Bulb Type (LED / Incandescent): ");
-                            String bulbType = input.nextLine();
-
-                            //create lightAppliance object and add it
-                            LightAppliance light = new LightAppliance(name, watts, hours, bulbType);
-                            household.addAppliance(light);
-                        }else if (typeChoice)
+                        System.out.println("");
+                        break;
                          
                     case 2:
-                        System.out.println("Enter appliance name: ");
-                        String name = scanner.nextLine();
-                        
-                        System.out.println("Enter Power Rating (Watts):  ");
-                        double watts = Double.parseDouble(input.nextLine());
-                        
-                        System.out.println("Enter Estimated Daily Usage (Hours):  ");
-                        double hours = Double.parseDouble(input.nextLine());
-                        
-                        System.out.println("Enter Cooling Type (Fan / AC): ");
-                        String coolingType = input.nextLine();
-                        
-                        //create coolingAppliance object and add it
-                        CoolingAppliance cooler = new CoolingAppliance(name, watts, hours, coolingType);
-                        household.addAppliance(light);
+                        System.out.println("");
                         break;
 
                     case 3:
