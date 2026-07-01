bf863d5b38a3e6057a79e345e53713ab37d28998
bongd <bongd@LAPTOP-VQTF7THH.student.taylors.edu.my>
2026-07-01 16:48:01 +0800

    light

---
+++ src/main/java/com/mycompany/oopgroupproject/LightAppliance.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oopgroupproject;
+
+/**
+ *
+ * @author bongd
+ */
+public class LightAppliance {
+    
+    /**
+     *
+     */
+    public class LightAppliance extends Appliance {
+
+    private String bulbType;
+
+    public LightAppliance(String name, double powerWatts, double hoursPerDay, String bulbType) {
+        super(name, powerWatts, hoursPerDay);
+        this.bulbType = bulbType;
+    }
+
+    public String getBulbType() {
+        return bulbType;
+    }
+
+    public void setBulbType(String bulbType) {
+        this.bulbType = bulbType;
+    }
+
+    @Override
+    public String getType() {
+        return "Lighting Appliance";
+    }
+
+    @Override
+    public double energyConsump() {
+        return (getPowerWatts() * getHoursPerDay()) / 1000.0;
+    }
+
+    @Override
+    public void displayAppliance() {
+        super.displayAppliance();
+        System.out.println("Bulb Type: " + bulbType);
+    }
+}
+}
