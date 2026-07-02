2230c700a68eb14bf8eb9fec69841cc8f40c7160
User <User@5CD323HWZT.student.taylors.edu.my>
2026-07-02 23:14:00 +0800

    qwerty

--- src/main/java/com/mycompany/oopgroupproject/application.java
+++ src/main/java/com/mycompany/oopgroupproject/application.java
@@ -8,30 +8,30 @@ package com.mycompany.oopgroupproject;
  *
  * @author bongd
  */
+import java.util.ArrayList;
 public class application {
+    static ArrayList<Appliance> appliances = new ArrayList<>();
     public static void main(String[] args) {
-      System.out.println("\n--- [3] VIEW ALL APPLIANCES ---");
-    System.out.println("Total Registered Devices: " + appliances.size());
+        
+        System.out.println("\n--- [3] VIEW ALL APPLIANCES --- ");
+        System.out.println("Total Registered Devices: " + appliances.size() + " ");
+        System.out.println("------------------------------------------------------------------------------------------------------ ");
+        System.out.printf("%-8s %-10s %-25s %-13s %-20s%n", "ID", "Type", "Appliance Name", "Rating (W)", "Config Details");
+        System.out.println("------------------------------------------------------------------------------------------------------- ");
 
-    System.out.println("------------------------------------------------------------------------------------------------------");
-    System.out.printf("%-12s %-12s %-30s %-15s %-25s\n",
-            "ID", "Type", "Appliance Name", "Rating (W)", "Config Details");
-    System.out.println("------------------------------------------------------------------------------------------------------");
-
-    if (appliances.isEmpty()) {
-        System.out.println("No appliances registered yet.");
-    } else {
-        for (Appliance appliance : appliances) {
-            System.out.printf("%-12s %-12s %-30s %-15s %-25s\n",
-                    appliance.getId(),
-                    appliance.getType(),
-                    appliance.getName(),
-                    appliance.getPowerRating() + "W",
-                    appliance.getConfigDetails());
+        if (appliances.isEmpty()) {
+            System.out.println("No appliances registered yet.");
+        } else {
+            for (Appliance appliance : appliances) {
+                System.out.printf("%-8s %-10s %-25s %-13s %-20s%n",
+                        appliance.getId(),
+                        appliance.getType(),
+                        appliance.getName(),
+                        (int)appliance.getPowerWatts() + "W",
+                        appliance.getConfigDetails());
+            }
         }
+        System.out.println("----------------------------------------------------------------------------- -------------------------------");
     }
+}
 
-    System.out.println("------------------------------------------------------------------------------------------------------");
-}
-    }
-}
